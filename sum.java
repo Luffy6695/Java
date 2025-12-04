ce5583e314455477be5bee62620cd912aaaa2da6
Sumanth Yadav <sumanthyadav216@gmail.com>
2025-12-04 21:47:50 +0530

    Modified code

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sum {
+    public static void main(String[] args){
+        int a,b,sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the vlue of a:");
+        a = sc.nextInt();
+        System.out.println("enter the value of b:-");
+        b = sc.nextInt();
+        sum = a + b;
+        System.err.println(sum);
+        sc.close();
+        
+    }
+}
